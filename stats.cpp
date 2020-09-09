64f36522fc4d285b4e60e7ed4723c11cca46ec3c
Dikshit <sharma.dikshit001@gmail.com>
2020-09-09 22:28:30 +0530

    Did the required changes

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,18 @@
 #include "stats.h"
+#include <numeric>
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<double> &vec) {
+    //base condition
+    Stats obj;
+    double sum = 0;
+
+    if(vec.size()== 0) return obj;
+    accumulate(vec.begin(), vec.end(), sum);
+
+    obj.max = * max_element(vec.begin(), vec.end());
+    obj.min = * min_element(vec.begin(), vec.end());
+    obj.avg = sum / vec.size();
+
+    return obj;
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,17 @@
 #include <vector>
+#include <math.h>
+
+class Stats {
+    private:
+        double max, min, average;
+    public:
+        Stats() {
+            max = nan("null");
+            min = nan("null");
+            average = nan("null");
+        }
+};
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector <double> &);
 }
